58a798e1f8922915f864c68bc329eeb6dab3a799
Matthew Vincent <matt.vincent@jax.org>
2015-07-30 15:18:01 -0400

    v0.1.1

--- g2gtools/__init__.py
+++ g2gtools/__init__.py
@@ -14,7 +14,7 @@ from .vcf2chain import vcf2chain
 from .bins import bins
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Matthew Vincent, The Jackson Laboratory'
 __email__ = 'matt.vincent@jax.org'
 
