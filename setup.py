f93a0e70b99ce1068511be285786398e0390ab2d
kbchoi-jax <kb.choi@jax.org>
2018-05-17 18:35:23 -0400

    [kbchoi] Release ver. 0.2.0

--- setup.py
+++ setup.py
@@ -53,10 +53,10 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7'
-        "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
     test_suite='tests',
