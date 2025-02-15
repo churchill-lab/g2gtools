452847244cd12fae340263df7f7c85e042d6f81e
mattjvincent <matt.vincent@jax.org>
2025-02-15 10:54:04 -0500

    added region file (missing from last commit)

---
+++ src/g2gtools/region.py
@@ -0,0 +1,248 @@
+# standard library imports
+import argparse
+import re
+import sys
+
+# 3rd party library imports
+# none
+
+# local library imports
+from g2gtools.exceptions import G2GRegionError
+#from g2gtools.vci import VCIFile
+#from g2gtools.g2g_utils import get_logger
+
+#logger = get_logger('g2gtools')
+
+REGEX_REGION = re.compile(
+    r'([\w.-]*)\s*(-|:)?\s*(\d+)\s*(MB|M|K|)?\s*(-|:|)?\s*(\d+|)\s*(MB|M|K|)?',
+    re.IGNORECASE,
+)
+REGEX_REGION_CHR = re.compile(
+    r'(CHR|)*\s*([0-9]{1,2}|X|Y|MT)\s*(-|:)?\s*(\d+)\s*(MB|M|K|)?\s*(-|:|)?\s*(\d+|)\s*(MB|M|K|)?',
+    re.IGNORECASE,
+)
+
+
+def exit(message: str = '', parser: argparse.ArgumentParser = None) -> None:
+    """
+    Print message, help, and exit.
+
+    Args:
+        message: The message to print.
+        parser: The argument parser.
+    """
+    if parser:
+        if message:
+            parser.error(message)
+        else:
+            parser.error('Unknown error specified')
+    else:
+        if message:
+            sys.stderr.write(f'[g2gtools] Error: {message}\n')
+
+    sys.exit(1)
+
+
+#
+#
+# Region
+#
+#
+
+
+class Region(object):
+    """ """
+
+    def __init__(
+        self,
+        seq_id,
+        start=None,
+        end=None,
+        strand='+',
+        name=None,
+        original_base=0,
+    ):
+        if start:
+            end = end if end else start + 1
+        self.seq_id = seq_id
+        self._strand = None
+        self._start = None
+        self._end = None
+        self.original_base = original_base
+        self.strand = strand
+        self.start = start
+        self.end = end
+        self.name = name
+
+    @property
+    def strand(self):
+        return self._strand
+
+    @strand.setter
+    def strand(self, value):
+        if value is not None:
+            if value in ('+', '1', 1):
+                self._strand = '+'
+            elif value in ('-', '-1', -1):
+                self._strand = '-'
+            else:
+                raise G2GRegionError(
+                    f'Illegal value for strand {value}, must be +, -, 1, -1'
+                )
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        if value is not None:
+            if value < 0:
+                raise G2GRegionError(
+                    f'Illegal value for start {value}, start must be >= 0'
+                )
+            self._start = value
+
+    def get_start(self):
+        if self.original_base == 1:
+            return self.start
+        return self.start - 1
+
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, value):
+        if value is not None:
+            if self.start and value <= self.start:
+                raise G2GRegionError(
+                    f'Illegal value for end {value}, end must be >= start {self.start}'
+                )
+
+            self._end = value
+        else:
+            if self.start:
+                self._end = self.start + 1
+
+    def str(self):
+        if self.original_base == 1:
+            if self.name:
+                return f'{self.name} {self.seq_id}:{self.start+1}-{self.end} ({self.strand})'
+            else:
+                return f'{self.seq_id}:{self.start + 1}-{self.end} ({self.strand})'
+        else:
+            if self.name:
+                return f'{self.name} {self.seq_id}:{self.start}-{self.end} ({self.strand})'
+            else:
+                return f'{self.seq_id}:{self.start}-{self.end} ({self.strand})'
+
+    def __str__(self):
+        if self.original_base == 1:
+            if self.name:
+                return f'{self.name} {self.seq_id}:{self.start+1}-{self.end}'
+            else:
+                return f'{self.seq_id}:{self.start + 1}-{self.end}'
+        else:
+            if self.name:
+                return f'{self.name} {self.seq_id}:{self.start}-{self.end}'
+            else:
+                return f'{self.seq_id}:{self.start}-{self.end}'
+
+    def __repr__(self):
+        return f'{self.seq_id}:{self.start}-{self.end} ({self.strand})'
+
+
+def get_multiplier(factor):
+    """
+    Convert the factor into a number.
+
+    :param factor: the string 'mb', 'm', or 'k'
+    :return: 10000000, 1000000, 1000 or 1
+    """
+    if factor.lower() == 'mb':
+        return 10000000
+    elif factor.lower() == 'm':
+        return 1000000
+    elif factor.lower() == 'k':
+        return 1000
+
+    return 1
+
+
+def parse_region(location_str, base=0, name=None):
+    """
+    Parse a string and return a location.
+
+    Format expected is seqid|start|end, where | is some sort of delimiter.
+
+    If end is None, 1 based was requested.
+
+    :param location_str: a string representing a genomic location
+    :param base: a number 0 or 1
+    :return: a Region
+    """
+    loc_match = REGEX_REGION.match(str(location_str).strip().replace(',', ''))
+
+    one_base = False
+
+    if loc_match:
+        loc_groups = loc_match.groups()
+        identifier = loc_groups[0]
+        start_base = loc_groups[2]
+        start_mult = loc_groups[3]
+        end_base = loc_groups[5]
+        end_mult = loc_groups[6]
+
+        if not identifier:
+            raise G2GRegionError('Cannot parse location')
+
+        if start_base:
+            try:
+                start_base = int(start_base)
+            except ValueError:
+                raise G2GRegionError('Start position is not numeric')
+        else:
+            raise G2GRegionError('Cannot parse start position')
+
+        start = start_base
+
+        if start_mult:
+            if start_mult.lower() not in ['mb', 'm', 'k']:
+                raise G2GRegionError(f'Unknown quantifier: {start_mult}')
+            start = start_base * get_multiplier(start_mult)
+        else:
+            if base == 1:
+                start -= 1
+
+        if end_base:
+            try:
+                end_base = int(end_base)
+            except ValueError:
+                raise G2GRegionError('End position is not numeric')
+        else:
+            # special condition to allow for only 1 location to be retrieved
+            # or from this start base on
+
+            if not loc_groups[4] and not end_base and not end_mult:
+                # only 1 position requested
+                one_base = True
+            else:
+                raise G2GRegionError('Cannot parse end position')
+
+        if end_mult:
+            if end_mult.lower() not in ['mb', 'm', 'k']:
+                raise G2GRegionError(f'Unknown quantifier: {end_mult}')
+    else:
+        raise G2GRegionError(f"Cannot parse location '{location_str}'")
+
+    if one_base:
+        end = None
+    else:
+        end = end_base * get_multiplier(end_mult)
+
+    region = Region(identifier, start, end, '+', original_base=base, name=name)
+
+    return region
+
+
